f253d798cf21c4adf3eb9f4ef151a97167798601
Joe <zhangjoe03@gmail.com>
2022-10-27 21:18:35 -0400

    Added UIs

---
+++ app/src/main/java/com/example/project/CreateAccountStep2.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountStep2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_step2);
+    }
+}
