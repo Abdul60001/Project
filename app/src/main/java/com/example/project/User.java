b4a509f05e8757ca4c0adcb5a0e5ee618d7710eb
e-freayh <efrea059@uottawa.ca>
2022-10-28 14:29:14 -0400

    Updated User class

--- app/src/main/java/com/example/project/User.java
+++ app/src/main/java/com/example/project/User.java
@@ -1,2 +1,49 @@
-package com.example.project;public class User {
+package com.example.project;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String userType;
+
+    public User(String username, String password, String userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
 }
