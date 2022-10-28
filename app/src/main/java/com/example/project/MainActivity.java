43244009ed0af3a026add0460d5296c5a8f32b92
e-freayh <efrea059@uottawa.ca>
2022-10-28 14:43:39 -0400

    Added click listeners for MainActivity buttons

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput, passwordInput;
+    Button login, createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHandler dbHandler = new DBHandler(this);
+
+        usernameInput = findViewById(R.id.editTextTextEmailAddress);
+        passwordInput = findViewById(R.id.editTextTextPassword);
+        login = findViewById(R.id.button2);
+        createAccount = findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("test1");
+            }
+        });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("test2");
+            }
+        });
     }
+
+
 }
