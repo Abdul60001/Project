8c3981ce633ed84edb14d56f95b438ef58f5e2cb
Evan <evan@Evans-MacBook-Pro.local>
2022-10-31 15:56:54 -0400

    Added delete user functionality

--- app/src/main/java/com/example/project/ManageUsers.java
+++ app/src/main/java/com/example/project/ManageUsers.java
@@ -2,8 +2,11 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Button;
@@ -15,28 +18,50 @@ public class ManageUsers extends AppCompatActivity {
     Button manage,back;
     ListView usersListView;
 
-    ArrayList<User> userList;
+    ArrayList<User> usersList;
     DBHandler dbHandler;
-    String[] usernameList;
+    String[] usernameStringList;
+
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_users);
+
+        builder = new AlertDialog.Builder(this);
+
         manage = findViewById(R.id.button10);
         back = findViewById(R.id.button13);
         usersListView = findViewById(R.id.listview1);
 
         dbHandler = new DBHandler(this);
-        userList = new ArrayList<>();
+        usersList = new ArrayList<>();
         syncUserList();
-        usernameList = getUsernamesFromUserList();
-
-        ArrayAdapter<String> usernameListAdapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, R.id.textView10, usernameList);
-        usersListView.setAdapter(usernameListAdapter);
+        syncUsersListView();
 
         back.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {goToAdminStarter();}
+        });
+
+        usersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                User selectedUser = usersList.get(position);
+                builder.setMessage("Delete user " + selectedUser.getUsername() + "?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dbHandler.deleteUserById(selectedUser.getId());
+                                syncUserList();
+                                syncUsersListView();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {}
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
         });
 
     }
@@ -47,22 +72,29 @@ public class ManageUsers extends AppCompatActivity {
     }
 
     private void syncUserList() {
-        userList.clear();
+        usersList.clear();
         Cursor cursor = dbHandler.getUsers();
         while (cursor.moveToNext()) {
-            userList.add(new User(Integer.valueOf(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3)));
+            usersList.add(new User(Integer.valueOf(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3)));
         }
         cursor.close();
+
+        getUsernamesFromUserList();
     }
 
-    private String[] getUsernamesFromUserList() {
-        String[] result = new String[userList.size()];
+    private void getUsernamesFromUserList() {
+        String[] result = new String[usersList.size()];
         int counter=0;
-        for(User u: userList) {
+        for(User u: usersList) {
             result[counter] = u.getUsername();
             counter++;
         }
-        return result;
+        usernameStringList = result;
+    }
+
+    private void syncUsersListView() {
+        ArrayAdapter<String> usernameListAdapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, R.id.textView10, usernameStringList);
+        usersListView.setAdapter(usernameListAdapter);
     }
 
 }
