a7e62110c39c969ff89804e424b7bf5faf9deb5b
Evan <evan@Evans-MacBook-Pro.local>
2022-11-28 16:22:06 -0500

    Added view list students for instructor

--- app/src/main/java/com/example/project/ViewEnrolledStudents.java
+++ app/src/main/java/com/example/project/ViewEnrolledStudents.java
@@ -3,8 +3,10 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -15,7 +17,7 @@ public class ViewEnrolledStudents extends AppCompatActivity {
     Button back;
     ListView studentsListView;
 
-    ArrayList<Course> studentsList;
+    ArrayList<User> studentsList;
     DBHandler dbHandler;
     String[] studentsStringList;
 
@@ -33,6 +35,9 @@ public class ViewEnrolledStudents extends AppCompatActivity {
         currentUser = (User) getIntent().getSerializableExtra("current_user");
         selectedCourse = (Course) getIntent().getSerializableExtra("selected_course");
 
+        syncStudents();
+        syncCoursesListView();
+
         back.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {goToViewCoursesInstructor();}
         });
@@ -42,5 +47,40 @@ public class ViewEnrolledStudents extends AppCompatActivity {
         Intent intent = new Intent(this,ViewCoursesInstructor.class);
         intent.putExtra("current_user", currentUser);
         startActivity(intent);
+    }
+
+    private void syncStudents(){
+        studentsList.clear();
+        Cursor cursor = dbHandler.getEnrolledUsersByCourseId(selectedCourse.getId());
+        ArrayList<Integer> userIds = new ArrayList<Integer>();
+        while(cursor.moveToNext()){
+            userIds.add(cursor.getInt(0));
+        }
+
+        for (int i : userIds){
+            Cursor c = dbHandler.getUsersById(i);
+            if (c.moveToNext()) {
+                studentsList.add(new User(Integer.valueOf(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3)));
+            }
+            c.close();
+        }
+        cursor.close();
+
+        getUsernamesFromStudentsList();
+    }
+
+    private void getUsernamesFromStudentsList() {
+        String[] result = new String[studentsList.size()];
+        int counter=0;
+        for(User u: studentsList) {
+            result[counter] = u.getUsername();
+            counter++;
+        }
+        studentsStringList = result;
+    }
+
+    private void syncCoursesListView() {
+        ArrayAdapter<String> usernameListAdapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, R.id.textView10, studentsStringList);
+        studentsListView.setAdapter(usernameListAdapter);
     }
 }
